8c8678b7e8651d2ddef12d76a67caa0fc6748a57
krosengr4 <rosenkev4@gmail.com>
2023-11-28 09:07:41 -0600

    Finished seed.js

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,4 +1,24 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, BlogPost } = require('../models');
 
-const userData = require('./userData.json')
+const userData = require('./userData.json');
+const blogPostData = require('./blogPostData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    for ( const BlogPost of blogPostData) {
+        await BlogPost.create({
+            ...BlogPost, 
+            user_id: users[Math.floor(Math.random()* users.length)].id,
+        });
+    }
+    process.exit(0);
+};
+
+seedDatabase();
