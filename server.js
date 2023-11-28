0996e51beee5b0487217b356c674c988bbbcbc8d
krosengr4 <rosenkev4@gmail.com>
2023-11-28 14:30:22 -0600

    Finished models, started on routes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 const routes = require ('./controllers');
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const app = express();
