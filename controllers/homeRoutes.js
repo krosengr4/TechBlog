469e81948b61e54be84524edc4b5a17047684328
krosengr4 <rosenkev4@gmail.com>
2023-11-29 06:42:15 -0600

    Started on login js, edited stuff in handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -64,7 +64,7 @@ router.get('/', async (req, res) => {
     }
   });
 
-  router.get('login', (req, res) => {
+  router.get('/login', (req, res) => {
     if (req.session.logged_in) {
       res.redirect('/profile');
       return; 
---
+++ public/js/login.js
@@ -0,0 +1,16 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+}
+
+
+
+
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
