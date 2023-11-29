c60dcfe38728655eb978b177ef61c072c23d4c87
krosengr4 <rosenkev4@gmail.com>
2023-11-29 06:13:58 -0600

    completed handlebars for profile

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
 
         // Pass serialized data and session flag into template
     res.render('homepage', { 
-        projects, 
+        blogPosts, 
         logged_in: req.session.logged_in 
         });
     } catch (err) {
