2c78b7e12f0c19b89d21bb76ed1c20bc5c622ac9
krosengr4 <rosenkev4@gmail.com>
2023-12-05 21:10:52 -0600

    coded to get the signup button working

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,9 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
+    alert(document.querySelector('#password-login').value.trim, "login btn clicked");
     
     const userName = document.querySelector('#user-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
-    
     if (userName && password) {
         const response = await fetch('/api/users/login', {
             method: 'POST',
@@ -21,6 +21,28 @@ const loginFormHandler = async (event) => {
 
 const signupFormHandler = async (event) => {
     event.preventDefault();
+
+    alert('you clicked sign in');
+
+    const userName = document.querySelector('#user-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    const name = document.querySelector('#name-signup').value.trim();
+
+    
+
+    if (userName && password && name) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ userName, password, name }),
+            headers: { 'Content-Type': 'application/json' },
+        })
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        };
+    };
 
     console.log("You clicked signup");
 
