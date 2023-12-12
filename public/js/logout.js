d8997aee765e4eea839b82aec2eb46f11b9a20da
krosengr4 <rosenkev4@gmail.com>
2023-12-12 00:59:58 -0600

    Coded logout function and styled header + Login page

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,5 +1,14 @@
-const loginFormHandler = async (event) => {
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
 
-    const userName = document.querySelector('#user-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-}
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
