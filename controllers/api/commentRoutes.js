a2fa1d0a85e66861a905c812fe4ec8675b0252ec
krosengr4 <rosenkev4@gmail.com>
2023-12-24 03:36:08 -0600

    finish route for comment create and delete

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,7 +1,7 @@
 //! This file contains routes to create and delete a comment
 
 const router =require("express").Router();
-const { Comment } = require('../../models/Comment');
+const { Comment } = require('../../models');
 const withAuth = require('../../utils/withAuth');
 
 // create a comment
@@ -17,3 +17,26 @@ router.post("/", async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+// delete a comment
+router.delete("/:id",isAuth, async (req, res) => {
+    try {
+      const commentData = await Comment.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if(!commentData) {
+        res.status(400).json({ message: "no comments found with this id" });
+        return;
+      }
+
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  module.exports = router;
