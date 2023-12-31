28bfbdd2e8f875f50a0ad4b6a23bbc64974c20a8
krosengr4 <rosenkev4@gmail.com>
2023-12-30 23:10:55 -0600

    change the sql connection

--- config/connection.js
+++ config/connection.js
@@ -9,10 +9,9 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     sequelize = new Sequelize(
-        // process.env.DB_NAME,
-        // process.env.DB_USER,
-        // process.env.DB_PASSWORD,
-        "techblog_db", "root", "611854kr",
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
         {
             host: 'localhost', //!<--- "127.0.0.1" or "localhost"
             dialect: 'mysql',
